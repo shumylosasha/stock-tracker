3f6eb3c485c249c529f610047d8493f3261bb9f8
shumylosasha <shumylo.s@gmail.com>
2021-06-30 19:20:33 +0300

    end

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
 
 
   devise :database_authenticatable, :registerable,
@@ -23,10 +25,40 @@ class User < ApplicationRecord
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
   end
 
-
   def full_name
     return "#{first_name} #{last_name}" if first_name || last_name
     "Anonymous"
+  end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches("first_name", param)
+  end
+
+  def self.last_name_matches(param)
+    matches("last_name", param)
+  end
+
+  def self.email_matches(param)
+    matches("email", param)
+  end
+
+  def self.matches(filed_name, param)
+    where("#{filed_name} like ?", "%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
+
+  def not_friend_with?(user_id)
+    !self.friends.where(id: user_id).exists?
   end
 
 end
